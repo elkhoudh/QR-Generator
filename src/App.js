3c376340d60bf8f46805f68e185a7e3a6f5b5f2e
Hamza Elkhoudiri <elkhoudh@gmail.com>
2018-11-13 20:22:37 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    url: "",
+    isSubmitted: false,
+    res: ""
+  };
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value }, function() {
+      this.createQrCode(this.state.url);
+    });
+  };
+
+  createQrCode = url => {
+    const proxyUrl = "https://cors-anywhere.herokuapp.com/",
+      targetUrl = `https://www.qrtag.net/api/qr_4.png?url=${url}`;
+    fetch(proxyUrl + targetUrl).then(res => {
+      this.setState({ res: res.url.substring(36) });
+    });
+  };
   render() {
+    const { res } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input
+          name="url"
+          value={this.state.url}
+          onChange={this.handleChange}
+          placeholder="Enter Url"
+        />
+        {!res ? (
+          "Please Enter URL to Generate QR Code"
+        ) : (
+          <img src={res} alt="..." />
+        )}
       </div>
     );
   }
